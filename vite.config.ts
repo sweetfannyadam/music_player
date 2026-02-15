ce8d85e4b9bd47a2d20cfc9ce41ef0824761e0f9
elizabeth <elizabethclarap@gmail.com>
2026-02-15 12:49:07 +0700

    5th

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/music-player/',
+  base: '/music_player/',
   plugins: [react()],
   server: {
     port: 3000,
